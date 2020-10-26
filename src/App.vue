b3dc9a0f2cf2227f79f7f54408cb8fe17e01d713
dominic_flocea <dominic.flocea@nitroagility.com>
2020-10-26 22:32:29 +0200

    Restructured project

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,8 @@
 <template>
   <div id="app">
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <a class="navbar-brand" href="#">⛅ Message Board</a>
+  </nav>
     <router-view/>
   </div>
 </template>
