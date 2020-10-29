e4871a02ba04c91b07d43b768d501753998cfeb8
dominatorfaf <dominicflocea.dev@gmail.com>
2020-10-29 16:24:38 +0200

    Loading messages onto the page

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,7 @@
     <nav class="navbar navbar-expand-lg navbar-light bg-light">
     <a class="navbar-brand" href="#">⛅ Message Board</a>
   </nav>
-    <router-view/>
+    <router-view class="container"/>
   </div>
 </template>
 
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,10 +1,43 @@
 <template>
   <div>
+    <form>
+    <div class="list-unstyled"  v-for="message in messages" :key="message._id">
+      <li class="media">
+        <img v-if="message.imageURL" :src="message.imageURL" class="mr-3" :alt="message.subject">
+        <div class="media-body">
+          <h5 class="mt-0 mb-1">{{message.username}} | {{message.subject}}</h5>
+          {{message.message}}
+        </div>
+      </li>
+      <hr>
+    </div>
   </div>
 </template>
 
 <script>
+const API_URL = 'http://localhost:5000/messages';
+
 export default {
   name: 'Home',
+  data: () => ({
+    messages: [],
+  }),
+  mounted() {
+    fetch(API_URL).then((response) => response.json()).then((result) => {
+      console.log(result);
+      this.messages = result;
+    });
+  },
 };
 </script>
+
+<style>
+hr {
+  border-top: 1px solid gray;
+}
+
+img {
+  max-width: 300px;
+  height: auto;
+}
+</style>
